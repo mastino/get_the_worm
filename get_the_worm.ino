183bf4a2d81e7c44969cc2f8e9e1448235b08d48
Brian Gravelle <bgravelle@zagmail.gonzaga.edu>
2015-09-26 23:29:24 -0700

    v0.1 - quick and dirty but running

--- get_the_worm.ino
+++ get_the_worm.ino
@@ -7,22 +7,89 @@
  * Brian J Gravelle (bgravelle5@gmail.com)
  *
  * First started: Sept. 23 2015
- * Last modified: Sept. 23 2015
+ * Last modified: Sept. 26 2015
 */
 
 #include <TimerOne.h>
 
 #define LED_PIN 2
-#define START_SEC 30
-#define START_MIN 0
-#define START_HOU 0
-#define ALARM_MIN 1
-#define ALARM_HOU 0
+#define BUT_PIN 7
+#define BUZ_PIN 4
+
+#define START_SEC 0
+#define START_MIN 25
+#define START_HOU 23
+#define ALARM_MIN 30
+#define ALARM_HOU 6
+//#define PUSH_TIME 300000 //5 minutes in milliseconds
+#define PUSH_TIME 60000 //1 minute in milliseconds
 
 char second, minute, hour;
 bool is_alarm; 
+bool is_half_count = false;
+
+void increment_time();
+
+//only starts alarm
+void check_alarm();
+
+void setup() {
+
+  pinMode(LED_PIN, OUTPUT); 
+  digitalWrite(BUZ_PIN, LOW);
+  pinMode(BUT_PIN, INPUT); 
+  pinMode(BUZ_PIN, OUTPUT);
+  digitalWrite(BUZ_PIN, LOW);   
+  
+  Timer1.initialize(500000); // Period = 0.5 sec / Freq = 20Hz
+  Timer1.attachInterrupt( timer_isr );
+  
+  second = START_SEC;
+  minute = START_MIN;
+  hour   = START_HOU;
+  
+}
  
+void loop() {
+
+   unsigned long time;
+
+   if(is_alarm) {
+      digitalWrite(LED_PIN, HIGH);
+      digitalWrite(BUZ_PIN, HIGH);
+      while(digitalRead(BUT_PIN) != HIGH);
+      digitalWrite(BUZ_PIN, LOW);
+
+      time = millis();
+      do {
+         while(digitalRead(BUT_PIN) != HIGH) {
+           time = millis();
+           digitalWrite(BUZ_PIN, HIGH);
+           digitalWrite(LED_PIN, HIGH);
+         }
+         digitalWrite(BUZ_PIN, LOW);
+         digitalWrite(LED_PIN, HIGH);
+         delay(100);
+         digitalWrite(LED_PIN, LOW);
+         delay(100);
+
+      } while( (millis() - time) < PUSH_TIME );
+      is_alarm = false;
+   }
+
+}
+ 
+
+void timer_isr() {
+   is_half_count = !is_half_count;
+   if(!is_half_count) {
+      increment_time();
+      check_alarm();
+   }
+}
+   
 void increment_time() {
+   
    second++;
    if(second >= 60) {
       second = 0;
@@ -35,6 +102,7 @@ void increment_time() {
    if(hour >= 24) {
       hour = 0;
    }  
+
 }
 
 //only starts alarm
@@ -44,25 +112,3 @@ void check_alarm() {
      is_alarm = true;
      
 }
-
-void setup() {
-
-  pinMode(LED_PIN, OUTPUT);    
-  
-  Timer1.initialize(1000000); // Period = 1 sec / Freq = 10Hz
-  Timer1.attachInterrupt( timer_isr );
-  
-  second = START_SEC;
-  minute = START_MIN;
-  hour   = START_HOU;
-  
-}
- 
-void loop() {
-  
-}
- 
-
-void timer_isr() {
-   increment_time();
-   check_alarm();
