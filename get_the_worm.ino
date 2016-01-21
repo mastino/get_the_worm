d7ffe3aa4c342cbaa794f15e65d74bc492998d21
Brian Gravelle <bgravelle@zagmail.gonzaga.edu>
2016-01-20 21:15:36 -0800

    trying to fix lcd character spew bug

--- get_the_worm.ino
+++ get_the_worm.ino
@@ -17,9 +17,9 @@
 #define BUT_PIN 7
 #define BUZ_PIN 4
 
-#define START_SEC 50
-#define START_MIN 59
-#define START_HOU 6
+#define START_SEC 00
+#define START_MIN 10
+#define START_HOU 9
 #define ALARM_MIN 0
 #define ALARM_HOU 7
 #define PUSH_TIME 30000 //5 minutes in milliseconds
@@ -41,6 +41,7 @@ void check_alarm();
 void ring_alarm_hold();
 void ring_alarm_pushup();
 void do_excercise(char* message, unsigned long ex_time);
+void wait_for_button();
 void increment_time();
 void print_time();
 
@@ -52,7 +53,7 @@ void setup() {
   pinMode(BUZ_PIN, OUTPUT);
   digitalWrite(BUZ_PIN, LOW);   
   
-  Timer1.initialize(500000); // Period = 0.5 sec / Freq = 20Hz
+  Timer1.initialize(499900); // Period = 0.5 sec / Freq = 20Hz
   Timer1.attachInterrupt( timer_isr );
   
   second = START_SEC;
@@ -82,25 +83,39 @@ void timer_isr() {
 
 void ring_alarm_pushup(){
   
-   digitalWrite(LED_PIN, HIGH);
-   digitalWrite(BUZ_PIN, HIGH);
-   while(digitalRead(BUT_PIN) != HIGH);
-   digitalWrite(BUZ_PIN, LOW);
- 
+   wait_for_button(); 
    do_excercise("Pushups GO!", ONE_MIN);
-   digitalWrite(LED_PIN, HIGH);
-   digitalWrite(BUZ_PIN, HIGH);
-   while(digitalRead(BUT_PIN) != HIGH);
-   digitalWrite(BUZ_PIN, LOW);
-   
-   do_excercise("Plank", THIRTY_S);
-   digitalWrite(LED_PIN, HIGH);
-   digitalWrite(BUZ_PIN, HIGH);
-   while(digitalRead(BUT_PIN) != HIGH);
-   digitalWrite(BUZ_PIN, LOW);
-   
+   wait_for_button();   
+   do_excercise("Forward Plank", THIRTY_S);
+   wait_for_button();   
+   do_excercise("L Side Plank", THIRTY_S);
+   wait_for_button();   
+   do_excercise("R Side Plank", THIRTY_S);
+   wait_for_button(); 
+   do_excercise("Pushups GO!", ONE_MIN);
+   wait_for_button();   
+   do_excercise("Forward Plank", THIRTY_S);
+   wait_for_button();   
+   do_excercise("L Side Plank", THIRTY_S);
+   wait_for_button();   
+   do_excercise("R Side Plank", THIRTY_S);
+   wait_for_button(); 
+   do_excercise("Pushups GO!", ONE_MIN);
+   wait_for_button();    
+  
    is_alarm = false;
+   lcd.clear();
+   print_time();
+   print_alarm();
+   digitalWrite(LED_PIN, LOW);
 
+}
+
+void wait_for_button() {
+   digitalWrite(LED_PIN, HIGH);
+   digitalWrite(BUZ_PIN, HIGH);
+   while(digitalRead(BUT_PIN) != HIGH);
+   digitalWrite(BUZ_PIN, LOW);
 }
 
 void do_excercise(char* message, unsigned long ex_time) {
@@ -108,9 +123,9 @@ void do_excercise(char* message, unsigned long ex_time) {
   
    time = millis();
    lcd.clear();
+   lcd.setCursor(0, 1);
+   lcd.print(message);
    while( (millis() - time) < ex_time ) {
-     lcd.setCursor(0, 1);
-     lcd.print(message);
      delay(100);
      digitalWrite(LED_PIN, HIGH);
      delay(100);
@@ -167,10 +182,7 @@ void increment_time() {
    if(hour >= 24) {
       hour = 0;
    }  
-   lcd.clear();
    print_time();
-   if(!is_alarm)
-     print_alarm();
 }
 
 
